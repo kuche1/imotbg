3164f91eb8bfd501f5487fcb4fd24e90fa01a302
kuche1 <znamsiparolata4esno@abv.bg>
2026-06-17 14:41:03 +0300

    update

--- main.go
+++ main.go
@@ -2,17 +2,35 @@ package main
 
 import (
 	"fmt"
+	"slices"
 
 	"github.com/kuche1/imotbg/config"
 	"github.com/kuche1/imotbg/extract"
+	"github.com/kuche1/imotbg/house"
 )
 
 func main() {
 	conf := config.NewConfig()
 
-	houses := extract.Main(conf)
+	houses := make([]*house.House, 0)
 
-	for house := range houses {
+	for house := range extract.Main(conf) {
+		houses = append(houses, house)
+	}
+
+	slices.SortFunc(
+		houses,
+		func(a *house.House, b *house.House) int {
+			if a.Area < b.Area {
+				return -1
+			}
+			if a.Area == b.Area {
+				return 0
+			}
+			return 1
+		})
+
+	for _, house := range houses {
 		repr := house.Sprintf()
 		fmt.Printf("%v\n", repr)
 	}
