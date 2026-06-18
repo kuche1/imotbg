f631d885957a45c9575550ee5ff5f710dd051495
kuche1 <znamsiparolata4esno@abv.bg>
2026-06-19 01:04:58 +0300

    update

--- define/boring.go
+++ define/boring.go
@@ -3,7 +3,7 @@ package define
 import "runtime"
 
 const NetCacheFolder = "net-cache"
-const NetRequestDelayMS = 2                           // IMPROVE: see if we can safely lower this value
+const NetRequestDelayMS = 3
 const NetCachedResponseValiditySec = 60 * 60 * 120000 // 120000 hours // TODO: revert to 12
 
 const LastPagePossible = 25
